fa3b6413ab7222e4e40a240d46fe43b713480156
Joel Kohn <jkohn@berkeley.edu>
2025-12-12 11:13:27 -0800

    Added card tokenization

--- card.py
+++ card.py
@@ -1,4 +1,9 @@
-
+from tokenizers.tokenize_oracle_text import tokenize_oracle_text
+from tokenizers.tokenize_mana_cost import tokenize_mana_cost
+from tokenizers.tokenize_simple_card_fields import tokenize_power, tokenize_toughness, tokenize_release_year, \
+    tokenize_rarity, tokenize_set_name
+from tokenizers.tokenize_name import tokenize_name
+from tokenizers.tokenize_type_line import tokenize_type_line
 
 class Card:
     def __init__(self, name, oracle_text, mana_cost, type_line, release_year, rarity, set_name, power=None, toughness=None):
@@ -40,3 +45,28 @@ class Card:
         if self.toughness:
             card_json["toughness"] = self.toughness
         return card_json
+
+    def generate_tokens(self, fields):
+        tokens = ''
+        for field in fields:
+            if field == 'name':
+                tokens += ''.join(tokenize_name(self.name))
+            elif field == 'oracle_text':
+                tokens += ''.join(tokenize_oracle_text(self.oracle_text))
+            elif field == 'mana_cost':
+                tokens += ''.join(tokenize_mana_cost(self.mana_cost))
+            elif field == 'type_line':
+                tokens += ''.join(tokenize_type_line(self.type_line))
+            elif field == 'release_year':
+                tokens += ''.join(tokenize_release_year(self.release_year))
+            elif field == 'rarity':
+                tokens += ''.join(tokenize_rarity(self.rarity))
+            elif field == 'set':
+                tokens += ''.join(tokenize_set_name(self.set_code))
+            elif field == 'power':
+                tokens += ''.join(tokenize_power(self.power))
+            elif field == 'toughness':
+                tokens += ''.join(tokenize_toughness(self.toughness))
+            else:
+                raise ValueError(f"Invalid field: {field}")
+        return tokens
---
+++ tests/test_tokenize_card.py
@@ -0,0 +1,75 @@
+import unittest
+import json
+import os
+
+from card import Card
+
+from special_tokens import begin_name_token, end_name_token, begin_oracle_text_token, end_oracle_text_token, \
+    begin_mana_cost_token, end_mana_cost_token, begin_type_line_token, subtype_break_token, end_type_line_token
+from tokenizers.tokenize_oracle_text import tokenize_oracle_text
+from tokenizers.tokenize_mana_cost import tokenize_mana_cost
+from tokenizers.tokenize_type_line import tokenize_type_line
+from tokenizers.tokenize_simple_card_fields import tokenize_power, tokenize_toughness, tokenize_release_year, \
+    tokenize_rarity, tokenize_set_name
+from tokenizers.tokenize_name import tokenize_name
+from tokenizers.tokenize_type_line import tokenize_type_line
+from tokenizers.tokenize_simple_card_fields import tokenize_power, tokenize_toughness, tokenize_release_year, \
+    tokenize_rarity, tokenize_set_name
+
+class TestTokenizeCard(unittest.TestCase):
+    def test_tokenize_card(self):
+        test_file = os.path.join(os.path.dirname(__file__), "test_data", "grizzly_bear.json")
+        with open(test_file, "r") as f:
+            data = json.load(f)
+        card = Card.from_json(None, data)
+        tokens = card.generate_tokens(["name", "oracle_text", "mana_cost", "type_line", "release_year", "rarity", "set", "power", "toughness"])
+        self.assertEqual(
+            tokens,
+                begin_name_token +
+                    '<name_char_G><name_char_r><name_char_i><name_char_z><name_char_z><name_char_l><name_char_y>' +
+                    '<name_char_ >' +
+                    '<name_char_B><name_char_e><name_char_a><name_char_r>' +
+                end_name_token +
+                begin_oracle_text_token +
+                end_oracle_text_token +
+                begin_mana_cost_token +
+                    '<mana_cost_1><mana_cost_G>' +
+                end_mana_cost_token +
+                begin_type_line_token +
+                    '<type_Creature>' +
+                    subtype_break_token +
+                    '<subtype_Bear>' +
+                end_type_line_token +
+                '<release_year_2007>' +
+                '<rarity_common>' +
+                '<set_10e>' +
+                '<power_2>' +
+                '<toughness_2>'
+        )
+
+    def test_card_with_oracle_text(self):
+        test_file = os.path.join(os.path.dirname(__file__), "test_data", "aang_air_nomad.json")
+        with open(test_file, "r") as f:
+            data = json.load(f)
+        card = Card.from_json(None, data)
+        tokens = card.generate_tokens(["name", "mana_cost", "oracle_text", "release_year", "rarity", "set", "power", "toughness", "type_line"])
+        self.assertEqual(
+            tokens,
+            ''.join(tokenize_name(card.name)) +
+            ''.join(tokenize_mana_cost(card.mana_cost)) +
+            ''.join(tokenize_oracle_text(card.oracle_text)) +
+            ''.join(tokenize_release_year(card.release_year)) +
+            ''.join(tokenize_rarity(card.rarity)) +
+            ''.join(tokenize_set_name(card.set_code)) +
+            ''.join(tokenize_power(card.power)) +
+            ''.join(tokenize_toughness(card.toughness)) +
+            ''.join(tokenize_type_line(card.type_line))
+        )
+    
+    def tokenize_nonexistent_field(self):
+        test_file = os.path.join(os.path.dirname(__file__), "test_data", "grizzly_bear.json")
+        with open(test_file, "r") as f:
+            data = json.load(f)
+        card = Card.from_json(None, data)
+        with self.assertRaises(ValueError):
+            tokens = card.generate_tokens(["nonexistent_field"])
