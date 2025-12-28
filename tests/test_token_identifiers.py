4095f669f78041ebb1b097e5cf95ffa98efcf986
Joel Kohn <jkohn@berkeley.edu>
2025-12-28 15:05:16 -0800

    Fixed tests (I hate dashes)

--- tests/test_token_identifiers.py
+++ tests/test_token_identifiers.py
@@ -7,9 +7,9 @@ from tokenizers.oracle_text_helper_functions.oracle_text_type_identifiers import
 class TestTokenIdentifiers(unittest.TestCase):
     def test_is_stat_change_token(self):
         self.assertTrue(is_stat_change_token('+1/+1'))
-        self.assertTrue(is_stat_change_token('−1/−1'))
-        self.assertTrue(is_stat_change_token('+X/−X'))
-        self.assertTrue(is_stat_change_token('−X/−X'))
+        self.assertTrue(is_stat_change_token('-1/-1'))
+        self.assertTrue(is_stat_change_token('+X/-X'))
+        self.assertTrue(is_stat_change_token('-X/-X'))
         self.assertTrue(is_stat_definition_token('1/1'))
         self.assertTrue(is_stat_definition_token('X/X'))
         self.assertTrue(is_stat_definition_token('1/X'))
--- tokenizers/oracle_text_helper_functions/oracle_text_type_identifiers.py
+++ tokenizers/oracle_text_helper_functions/oracle_text_type_identifiers.py
@@ -11,7 +11,7 @@ def is_stat_definition_token(token_string):
 def is_planeswalker_loyalty_ability_token(token_string, type_line):
     if 'Planeswalker' not in (type_line or ''):
         return False
-    return re.match(r'^(0|[\+-][1-9X]+)$', token_string) is not None
+    return re.match(r'^(0|[\+−][1-9X]+)$', token_string) is not None
 
 def is_planeswalker_loyaly_ability_colon_token(token_string, type_line):
     if 'Planeswalker' not in (type_line or ''):
